c5b442c80e6fa4b7f74d0c07e7800a737d87f268
mirmik <mirmikns@yandex.ru>
2022-10-06 21:14:31 +0300

    'fix dynsyst'

--- rxsignal/dynsystem.py
+++ rxsignal/dynsystem.py
@@ -1,21 +1,44 @@
 import numpy
 from rxsignal.observable import feedback_subject
+from scipy import linalg
+import numpy as np
 
-def dynsystem(g, A, B, init=None):
-	if init is None:
-		init = numpy.zeros(len(B))
 
-	x0 = feedback_subject()
-	x1 = x0.map(lambda a: numpy.matmul(A, a)) + g.map(lambda g: B*g)
-	x1.subscribe(lambda a: x0.on_next(a))	
-	x0.on_next(init)
-	return x1
+def matrix_discretization2(A, B, Step):
+    _A = linalg.expm(A * Step)
+    _B = linalg.inv(A).dot((_A - np.identity(A.shape[0])).dot(B))
+    return (_A, _B)
 
-def dynsystem2(g, A, B, C, D, init):
-	x0 = feedback_subject()
-	x1 = numpy.matmul(A, x0) + numpy.matmul(B, g)
-	y1 = numpy.matmul(C, x0) + numpy.matmul(D, g)
-	x1.subscribe(lambda a: x0.on_next(a))	
-	x0.on_next(init)
-	return y1
-	
+
+def matrix_discretization4(A, B, C, D, Step):
+    _A = linalg.expm(A * Step)
+    _B = linalg.inv(A).dot((_A - np.identity(A.shape[0])).dot(B))
+    _C = C
+    _D = D
+    return (_A, _B, _C, _D)
+
+
+def dynsystem(g, A, B, step, init=None):
+    Ad, Bd = matrix_discretization2(A, B, step)
+    if init is None:
+        init = numpy.zeros(len(B))
+
+    x0 = feedback_subject()
+    x1 = x0.map(lambda a: numpy.matmul(Ad, a)) + g.map(lambda g: Bd*g)
+    x1.subscribe(lambda a: x0.on_next(a))
+    x0.on_next(init)
+    return x1
+
+
+def dynsystem2(g, A, B, C, D, step, init=None):
+    Ad, Bd, Cd, Dd = matrix_discretization4(A, B, C, D, step)
+
+    if init is None:
+        init = numpy.zeros(len(B))
+
+    x0 = feedback_subject()
+    x1 = numpy.matmul(Ad, x0) + numpy.matmul(Bd, g)
+    y1 = numpy.matmul(Cd, x0) + numpy.matmul(Dd, g)
+    x1.subscribe(lambda a: x0.on_next(a))
+    x0.on_next(init)
+    return y1
--- rxsignal/observable.py
+++ rxsignal/observable.py
@@ -5,6 +5,7 @@ import operator
 import numpy
 import math
 
+
 class Observable:
     def __init__(self, o):
         self.o = o
@@ -13,7 +14,7 @@ class Observable:
         return Observable(self.o.pipe(reactivex.operators.map(foo)))
 
     def zip(self, oth):
-        return Observable(reactivex.zip(self.o, oth.o))        
+        return Observable(reactivex.zip(self.o, oth.o))
 
     def subscribe(self, *args, **kwargs):
         self.o.subscribe(*args, **kwargs)
@@ -35,10 +36,13 @@ class Observable:
 
     def add(self, arg):
         return self.op(operator.add, arg)
+
     def sub(self, arg):
         return self.op(operator.sub, arg)
+
     def mul(self, arg):
         return self.op(operator.mul, arg)
+
     def div(self, arg):
         return self.op(operator.truediv, arg)
 
@@ -47,33 +51,43 @@ class Observable:
 
     def radd(self, arg):
         return self.rop(operator.add, arg)
+
     def rsub(self, arg):
         return self.rop(operator.sub, arg)
+
     def rmul(self, arg):
         return self.rop(operator.mul, arg)
+
     def rdiv(self, arg):
         return self.rop(operator.truediv, arg)
 
     def sin(self):
         return self.map(lambda x: math.sin(x))
+
     def cos(self):
         return self.map(lambda x: math.cos(x))
 
     def __add__(self, oth):
         return self.add(oth)
+
     def __sub__(self, oth):
         return self.sub(oth)
+
     def __mul__(self, oth):
         return self.mul(oth)
+
     def __truediv__(self, oth):
         return self.div(oth)
 
     def __radd__(self, oth):
         return self.radd(oth)
+
     def __rsub__(self, oth):
         return self.rsub(oth)
+
     def __rmul__(self, oth):
         return self.rmul(oth)
+
     def __rtruediv__(self, oth):
         return self.rdiv(oth)
 
@@ -82,6 +96,7 @@ class Observable:
 
     def to_list(self, count):
         return self.o.range(0, count).to_list()
+
 
 class subject(Observable):
     def __init__(self, subject=None):
@@ -92,9 +107,10 @@ class subject(Observable):
     def on_next(self, val):
         self.o.on_next(val)
 
+
 class feedback_subject(subject):
     def __init__(self):
-        super().__init__(subject = reactivex.subject.ReplaySubject())
+        super().__init__(subject=reactivex.subject.ReplaySubject())
         self.q = queue.Queue()
         self.thr = threading.Thread(target=self.foo)
         self.thr.start()
@@ -111,8 +127,10 @@ class feedback_subject(subject):
 def rxinterval(d):
     return Observable(reactivex.interval(d))
 
-def rxrange(s,f):
-    return Observable(reactivex.range(s,f))
+
+def rxrange(s, f):
+    return Observable(reactivex.range(s, f))
+
 
 def zip(*x):
     return Observable(reactivex.zip(*[a.o for a in x]))
